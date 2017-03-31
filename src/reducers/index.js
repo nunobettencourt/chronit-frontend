ac1a6465b7a1abb1b7c665ab7cc51ae8c43f9c79
nuno.bettencourt <nuno.bettencourt@playtech.com>
2017-03-31 15:34:09 +0100

    CombineReducers update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import map from './map';
+import event from './event';
 
 
 const rootReducer = combineReducers({
     counter,
     map,
+    event
 });
 
 export default rootReducer;
