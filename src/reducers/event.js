13c94a8bc01a758a11ebe4821c50085c1dd9f382
nuno.bettencourt <nuno.bettencourt@playtech.com>
2017-03-31 15:08:43 +0100

    Event reducer update

--- src/reducers/event.js
+++ src/reducers/event.js
@@ -1,16 +1,16 @@
 import * as Type from 'constants/action-types';
 
-export default function event (state = {isFetching: false, url: undefined}, action) {
+export default function event (state = {isFetching: false, events: undefined}, action) {
 	switch (action.type) {
-		case Type.START_LOCATION_FETCH:
+		case Type.START_EVENT_LIST_FETCH:
 			return {
 				isFetching: true,
-				url: undefined
+				events: undefined
 			};
-		case Type.COMPLETE_LOCATION_FETCH:
+		case Type.COMPLETE_EVENT_LIST_FETCH:
 			return {
 				isFetching: false,
-				url: action.url
+				events: action.events
 			};
 		default:
 			return state;
